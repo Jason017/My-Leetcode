3cd465d86032963775ecbbd42fc07df47a7f61d6
Jason017 <jasonguan0107@gmail.com>
2021-11-14 02:23:46 -0500

    283

--- Easy/0283 Move Zeroes/Solution.py
+++ Easy/0283 Move Zeroes/Solution.py
@@ -17,3 +17,19 @@ class Solution:
                 
         for j in range(idx,n):
             nums[j] = 0
+
+    def moveZeros(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums or len(nums) == 0:
+            return 
+        
+        idx = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] != 0:
+                temp = nums[i]
+                nums[i] = nums[idx]
+                nums[idx] = temp
+                idx += 1
