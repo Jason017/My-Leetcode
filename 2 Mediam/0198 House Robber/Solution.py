de28d604a6ad7d77919f04c0d757f5a35c07f89e
Jason <jasonguan0107@gmail.com>
2021-11-21 12:06:39 -0500

    lc 198 sol2

--- 2 Mediam/0198 House Robber/Solution.py
+++ 2 Mediam/0198 House Robber/Solution.py
@@ -1,6 +1,24 @@
 from typing import List
 
 class Solution:
+    # Solution 1
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * n
+        dp[0], dp[1] = nums[0], max(nums[:2])
+
+        if n == 2:
+            return dp[1]
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        return dp[-1]
+
+    # Solution 2
     def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
         
