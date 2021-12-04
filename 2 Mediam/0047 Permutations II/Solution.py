ff4c1c6a8763d38ff83e2b2a65420802f48c3e77
Jason <jasonguan0107@gmail.com>
2021-12-04 15:20:52 -0500

    12/3

---
+++ 2 Mediam/0047 Permutations II/Solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def backtrack(self, nums, output, level=0):
+        if level == len(nums):
+            output.append(nums[:])
+        visited = set()
+        for i in range(level, len(nums)):
+            nums[i], nums[level] = nums[level], nums[i]
+            if nums[level] not in visited:
+                visited.add(nums[level])
+                self.backtrack(nums, output, level+1)
+            nums[i], nums[level] = nums[level], nums[i]
+
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        self.backtrack(nums, output)
+        return output
