da6d2d2ecda363b28f656b9232d0fc8ceb3d0e2a
Jason <jasonguan0107@gmail.com>
2021-12-11 22:41:32 -0500

    12/11

---
+++ 2 Mediam/0133 Clone Graph/Solution.py
@@ -0,0 +1,69 @@
+from collections import deque
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    # Solution 1: DFS + Recursion
+    # O(E+V) number of edges and vertices in the graph
+    # O(H) height of the graph
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        oldToNew = {node: Node(node.val)}
+
+        def dfs(n):
+            for nei in n.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    dfs(nei)
+                oldToNew[n].neighbors.append(oldToNew[nei])
+
+        dfs(node)
+        return oldToNew[node]
+
+
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        oldToNew = {}
+
+        def dfs(n):
+            if n in oldToNew:
+                return oldToNew[n]
+                
+            copy = Node(n.val)
+            oldToNew[node] = copy
+
+            for nei in n.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        
+        dfs(node)
+        return oldToNew[node]
+
+
+    # Solution 2: BFS
+    # O(E+V) number of edges and vertices in the graph
+    # O(W) width of the graph
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        
+        oldToNew = {}
+        q = deque([node])
+        oldToNew[node] = Node(node.val)
+        
+        while q:
+            n = q.popleft()
+            for nei in n.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[n].neighbors.append(oldToNew[nei])
+        return oldToNew[node]
+
--- 2 Mediam/0208 Implement Trie (Prefix Tree)/Solution.py
+++ 2 Mediam/0208 Implement Trie (Prefix Tree)/Solution.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-class TrieNode(object):
+class TrieNode:
     def __init__(self):
         self.nodes = defaultdict(TrieNode)
         self.isEnd = False
@@ -30,3 +30,15 @@ class Trie:
                 return False
             curr = curr.nodes[char]
         return True
+
+def main():
+    t = Trie()
+    t.insert("apple")
+    print(t.search("apple")) # True
+    print(t.search("app")) # False
+    print(t.startsWith("app")) # True
+    t.insert("app")
+    print(t.search("app")) # True
+
+if __name__ == "__main__":
+    main()
