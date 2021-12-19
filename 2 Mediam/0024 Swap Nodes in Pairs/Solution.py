f79a1ac4f0bad77664a766d2239b1bf7eb3274c9
Jason <jasonguan0107@gmail.com>
2021-12-18 19:18:40 -0500

    补

--- 2 Mediam/0024 Swap Nodes in Pairs/Solution.py
+++ 2 Mediam/0024 Swap Nodes in Pairs/Solution.py
@@ -6,14 +6,47 @@ class ListNode:
         self.next = next
         
 class Solution:
+    # Solution 1: Recursion
+    # O(N), O(N)
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
         
-        slow = head
-        fast = head.next
+        swapped = self.swapPairs(head.next.next)
+        nextNode = head.next
+        head.next = swapped
+        nextNode.next = head
+        return nextNode
+
+    # Solution 2: Simpler Recursion
+    # O(N), O(N)
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
         
-        slow.next = self.swapPairs(fast.next)
-        fast.next = slow
+        currNode = head
+        nextNode = head.next
+        currNode.next = self.swapPairs(nextNode.next)
+        nextNode.next = currNode
         
-        return fast
+        return nextNode
+
+    # Solution 3: Iteration
+    # O(N), O(N)
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = dummy = ListNode(-1, head)
+        
+        while head and head.next:
+            currNode = head
+            nextNode = head.next
+            
+            prev.next = nextNode
+            currNode.next = nextNode.next
+            nextNode.next = currNode
+            
+            prev = currNode
+            head = head.next
+            
+        return dummy.next
+
+    
---
+++ 2 Mediam/2095 Delete the Middle Node of a Linked List/Solution.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # Solution 1: Brute Force with Two Pointers
+    # O(N), O(1)
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1, head)
+        slow = fast = tmp = dummy
+        
+        n = 0
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            n += 1
+            
+        if not fast:
+            n -= 1
+            
+        while n:
+            tmp = tmp.next
+            n -= 1
+        tmp.next = tmp.next.next
+        return dummy.next
+
+    # Solution 2: Fast and Slow Pointers
+    # O(N), O(1)
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        if head and not head.next: return None
+        
+        prev = ListNode(0, head)
+        slow = fast = head
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev.next = slow.next
+        return head
