a03c6d6326134f40e07d8d57b943e0c77f622ab5
Jason <jasonguan0107@gmail.com>
2021-12-20 11:10:06 -0500

    12/20

---
+++ 2 Mediam/0078 Subsets/Solution.py
@@ -0,0 +1,43 @@
+from typing import List
+
+class Solution:
+    # Solution 1: Cascading
+    # O(N*2^N), O(N*2^N)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+        for num in nums:
+            output += [o+[num] for o in output]
+        return output
+    
+    # Solution 2: Backtracking
+    # O(N*2^N), O(N)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+
+        def backtrack(length, index=0, subset=[]):
+            if len(subset) == length:
+                output.append(subset[:])
+            for i in range(index, len(nums)):
+                subset.append(nums[i])
+                backtrack(length, i + 1, subset)
+                subset.pop()
+        
+        for i in range(len(nums)+1):
+            backtrack(i)
+        return output
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        
+        def dfs(index=0, subset=[]):
+            output.append(subset)
+            for i in range(index, len(nums)):
+                dfs(i + 1, subset + [nums[i]])
+        dfs()
+        return output
+
+
+sol = Solution()
+nums = [1,2,3]
+print(sol.subsets(nums))
+
