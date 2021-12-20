8153f18ff42aa797ce0d0637fe8669bac5174698
Jason <jasonguan0107@gmail.com>
2021-12-20 11:18:26 -0500

    200

--- 2 Mediam/0200 Number of Islands/Solution.py
+++ 2 Mediam/0200 Number of Islands/Solution.py
@@ -1,7 +1,8 @@
 from typing import List
+from collections import deque
 
 class Solution:
-    # Solution 1: Recursive DFS
+    # Solution 1: DFS Recursive Approach
     # O(M*N), O(M*N)
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid), len(grid[0])
@@ -22,4 +23,29 @@ class Solution:
                     dfs(i,j)
                     ans += 1
         return ans
+    
+    # Solution 2: BFS Iterative Approach
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m,n = len(grid), len(grid[0])
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        def bfs(i,j):
+            q = deque([(i,j)])
+            while q:
+                r, c = q.popleft()
+                for d in dirs:
+                    nr, nc = r + d[0], c + d[1]
+                    if not (nr in range(m) and nc in range(n) and grid[nr][nc] == '1'):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = '0'
         
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    bfs(i,j)
+                    ans += 1
+        
+        return ans
+
