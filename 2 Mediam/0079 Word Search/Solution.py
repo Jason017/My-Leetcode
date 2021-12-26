8fad75dc2c9d86e28e82366f57d3fd34b4fc382a
Jason <jasonguan0107@gmail.com>
2021-12-26 00:14:53 -0500

    12/25

--- 2 Mediam/0079 Word Search/Solution.py
+++ 2 Mediam/0079 Word Search/Solution.py
@@ -1,23 +1,39 @@
 from typing import List
 
 class Solution:
+    # Solution: DFS
+    # O(N*3^L), O(L), where NN is the number of cells in 
+    # the board and LL is the length of the word to be matched.
+
+    # For the backtracking function, initially we could have 
+    # at most 4 directions to explore, but further the choices 
+    # are reduced into 3 (since we won't go back to where we 
+    # come from). As a result, the execution trace after the 
+    # first step could be visualized as a 3-ary tree, each of 
+    # the branches represent a potential exploration in the 
+    # corresponding direction. Therefore, in the worst case,
+    # the total number of invocation would be the number of 
+    # nodes in a full 3-nary tree, which is about 3^L.
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        
-        def dfs(i,j,idx,word,board):
-            if idx == len(word):
+        m,n = len(board), len(board[0])
+        dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        def dfs(r,c,index):
+            if len(word) == index:
                 return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[idx] != board[i][j]:
+            if r not in range(m) or c not in range(n) or board[r][c] != word[index]:
                 return False
             
-            temp = board[i][j]
-            board[i][j] = ' '
-            res = dfs(i+1,j,idx+1,word,board) or dfs(i,j+1,idx+1,word,board) or dfs(i-1,j,idx+1,word,board) or dfs(i,j-1,idx+1,word,board)
-            board[i][j] = temp
-            return res
+            tmp = board[r][c]
+            board[r][c] = '#'
+            for dr, dc in dirs:
+                if dfs(r+dr, c+dc, index+1):
+                    return True
+            board[r][c] = tmp
+            return False
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if word[0] == board[i][j] and dfs(i,j,0,word,board):
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == word[0] and dfs(r,c,0):
                     return True
         return False
