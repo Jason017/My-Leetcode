aec32e22316ec965f91f88200e202dd0ed99b8d9
Jason <jasonguan0107@gmail.com>
2021-12-27 02:03:19 -0500

    12/26

--- 2 Mediam/0090 Subsets II/Solution.py
+++ 2 Mediam/0090 Subsets II/Solution.py
@@ -1,6 +1,8 @@
 from typing import List
 
 class Solution:
+    # Solution 1: Cascading Approach
+    # O(N*2^N), O(log(N))
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = [[]]
@@ -9,11 +11,36 @@ class Solution:
             temp = []
             for j in ans:
                 temp.append([i] + j)
-                # print("temp:",temp)
             ans.extend(temp)
-            # print(ans)
-        return(ans)
+        return ans
 
+
+    # Solution 2: BFS Backtracking Approach
+    # O(N*2^N), O(N)
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        nums.sort()
+        def dfs(index=0, subset=[]):
+            if subset not in output:
+                output.append(subset)
+            for i in range(index, len(nums)):
+                dfs(i + 1, subset + [nums[i]])
+        dfs()
+        return output
+
+    # Better Implementation
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        nums.sort()
+        def dfs(index=0, subset=[]):
+            output.append(subset)
+            for i in range(index, len(nums)):
+                if i != index and nums[i] == nums[i-1]:
+                    continue
+                dfs(i + 1, subset + [nums[i]])
+        dfs()
+        return output
+            
 
 sol = Solution()
 print(sol.subsetsWithDup([1,2,2,3]))
