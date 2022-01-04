69ead01ad7bf43b09761aebf06519f05f9fabcaa
Jason <jasonguan0107@gmail.com>
2022-01-03 20:39:23 -0500

    1/2

--- 2 Mediam/0015 3Sum/Solution.py
+++ 2 Mediam/0015 3Sum/Solution.py
@@ -1,6 +1,35 @@
 from typing import List
 
 class Solution:
+    # Solution 1: Navie Approach
+    # O(N^2), O(N)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        if not nums:
+            return ans
+        if len(nums) < 3:
+            return ans
+        
+        nums.sort()
+        for idx in range(len(nums)-2):
+            if idx > 0 and nums[idx-1] == nums[idx]:
+                continue
+            low, high = idx+1, len(nums)-1
+            while low < high and high > idx:
+                s=nums[idx]+nums[low]+nums[high]
+                if s==0 and [nums[idx], nums[low], nums[high]] not in ans:
+                    ans.append([nums[idx], nums[low], nums[high]])
+                    low+=1
+                    high-=1
+                elif s<0:
+                    low+=1
+                else:
+                    high-=1
+        return ans
+
+                
+    # Solution 2: Two Pointers
+    # O(N^2), O(N)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         if not nums or len(nums) < 3:
             return []
--- 2 Mediam/0034 Find First and Last Position of Element in Sorted Array/Solution.py
+++ 2 Mediam/0034 Find First and Last Position of Element in Sorted Array/Solution.py
@@ -1,4 +1,5 @@
 from typing import List
+from bisect import bisect_left, bisect_right
 
 class Solution:
     # Solution 1: Binary Search
@@ -32,12 +33,12 @@ class Solution:
         return [pos1, pos2]
 
     # Solution 2: Bisect
-    # O(log(N)), O(N)
+    # O(log(N)), O(1)
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums: return [-1,-1]
 
         low = bisect_left(nums, target)
         high = bisect_right(nums, target)-1        
-        if low not in range(len(nums)) or nums[low] != target:
+        if low < 0 or low > len(nums)-1 or nums[low] != target:
             return [-1,-1]
         return [low, high]
